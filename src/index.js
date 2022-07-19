82de9af19b5d3ecfa5b5e18399c94cee43349dad
mynkbis <bisht.surya@ymail.com>
2022-07-19 11:10:11 +0530

    version corrected

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+// const root = ReactDOM.React.render(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//       </BrowserRouter>
+      
+//   </React.StrictMode>
+// );
+
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      </BrowserRouter>
-      
-  </React.StrictMode>
+    <App />
+  </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
